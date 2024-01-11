3f44286ab5e5a8341cc9f8e205374403cf599fff
Marko Visnjic <markovisnjic12@gmail.com>
2024-01-11 16:01:33 -0500

    Added dictionary class

--- definition.py
+++ definition.py
@@ -1,7 +1,15 @@
+import pandas
+
+
 class Definition:
 
-    def __init__(self, term):
+    def __init__(self, term: str):
         self.term = term
 
     def get(self):
-        pass
+        df = pandas.read_csv('data.csv')
+        return tuple(df.loc[df['word'] == self.term]['definition'])
+
+
+if __name__ == "__main__":
+    print(Definition('chicken').get())
