4b221ac950cdf036ac2b4da6a45d52fb9eaffc92
Marko Visnjic <markovisnjic12@gmail.com>
2024-01-12 19:43:42 -0500

    Create navigation menu for Home

--- webapp/dictionary.py
+++ webapp/dictionary.py
@@ -8,24 +8,31 @@ class Dictionary:
     @classmethod
     def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
-        div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
-        jp.Div(a=div, text="Instant English Dictionary",
+
+        div = jp.Div(a=wp,
+                     classes="bg-gray-200 h-screen")
+
+        jp.Div(a=div,
+               text="Instant English Dictionary",
                classes="text-4xl m-2")
-        jp.Div(a=div, text="Get the definition of any English"
-                           "word instantly as you type.",
+
+        jp.Div(a=div,
+               text="Get the definition of any English"
+                    "word instantly as you type.",
                classes="text-lg")
 
-        input_div = jp.Div(a=div, classes="grid grid-cols-2")
+        input_div = jp.Div(a=div,
+                           classes="grid grid-cols-2")
 
-        output_div = jp.Div(a=div, classes="m-2 p-2 text-lg border-2 "
-                                           "border-gray-300 h-40")
+        output_div = jp.Div(a=div,
+                            classes="m-2 p-2 text-lg border-2 "
+                                    "border-gray-300 h-40")
 
         input_box = jp.Input(a=input_div,
                              placeholder="Type in a word here...",
                              classes="m-2 bg-gray-100 border-2 "
                                      "border-gray-200",
-                             outputdiv=output_div
-                             )
+                             outputdiv=output_div)
 
         input_box.on('input', cls.get_definition)
 
--- webapp/home.py
+++ webapp/home.py
@@ -1,20 +1,36 @@
 import justpy as jp
 
+
 class Home:
     path = "/"
 
-    def serve(self):
+    @classmethod
+    def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
-        div = jp.Div(a=wp, classes="bg-gray-200 "
-                                   "h-screen")
-        jp.Div(a=div, text="This is the about page!",
-               classes="text-4xl m-2")
-        jp.Div(a=div, text=("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum sapien sem, "
-                            "et semper dolor commodo porta. Sed ante tortor, consequat vitae tempor quis, interdum id "
-                            "elit. Cras accumsan justo at dui iaculis, eget placerat massa aliquet. Pellentesque et "
-                            "semper nulla, sodales lacinia ante. Aliquam efficitur nulla at porta lobortis. Nunc nunc "
-                            "ipsum, blandit ac urna sit amet, imperdiet dapibus ipsum. Pellentesque habitant morbi "
-                            "tristique senectus et netus et malesuada fames ac turpis egestas. Proin id ornare elit.\n"
-                            ), classes="text-lg")
+
+        layout = jp.QLayout(a=wp, view="hHh lpR fFf")
+        header = jp.QHeader(a=layout, elevated=True, classes="bg-primary text-white")
+        toolbar = jp.QToolbar(a=header)
+        drawer = jp.QDrawer(a=layout, show_if_above=True, v_mode="left", bordered=True, value=False)
+        button = jp.QButton(a=toolbar, dense=True, flat=True, round=True, icon="menu",click=cls.move_drawer,
+                            drawer=drawer)
+        jp.QToolbarTitle(a=toolbar, text="Instant Dictionary")
+        page_container = jp.QPageContainer(a=layout)
+
+        scroller = jp.QScrollArea(a=drawer, classes="fit")
+        qlist = jp.QList(a=scroller)
+        a_classes = "p-2 m-2 text-lg text-blue-400 hover:text-blue-700"
+        jp.A(a=qlist, text='Home', href="/", classes=a_classes)
+        jp.Br(a=qlist)
+        jp.A(a=qlist, text='Dictionary', href="/dictionary", classes=a_classes)
+        jp.Br(a=qlist)
+        jp.A(a=qlist, text='About', href="/about", classes=a_classes)
+        jp.Br(a=qlist)
+
+        jp.Div(a=page_container, text="HELLLLOOOOOOOO")
+
         return wp
 
+    @staticmethod
+    def move_drawer(widget, msg):
+        widget.drawer.value = not widget.drawer.value
